edd6371e04293663b365fed56505c0bf3396111e
Patrick Double <pat@patdouble.com>
2026-07-17 17:01:59 -0500

    Add pytest --ollama option

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,11 +12,23 @@ def pytest_configure(config):
     setattr(GeneratorConfig, "ollama_pull", noop)
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--ollama",
+        action="store_true",
+        default=False,
+        help="Run tests that require the Ollama."
+    )
+
+
 def pytest_runtest_setup(item):
     if "ollama" in item.keywords:
-        ollama_host = os.environ.get("OLLAMA_HOST", "127.0.0.1:11434")
-        try:
-            r = requests.get(f"http://{ollama_host}/api/tags", timeout=5)
-            r.raise_for_status()
-        except (requests.RequestException, ValueError):
-            pytest.skip(f"Skipping tests: Ollama is not available at http://{ollama_host}", allow_module_level=True)
+        if item.config.getoption("--ollama"):
+            ollama_host = os.environ.get("OLLAMA_HOST", "127.0.0.1:11434")
+            try:
+                r = requests.get(f"http://{ollama_host}/api/tags", timeout=5)
+                r.raise_for_status()
+            except (requests.RequestException, ValueError):
+                pytest.skip(f"Skipping tests: Ollama is not available at http://{ollama_host}", allow_module_level=True)
+        else:
+            pytest.skip("Test requires --ollama option to run.")
